bd2f1f1c0e802740f87fb90666054e8b61fb2dc3
Joshua Biggs <chiefjosh@gmail.com>
2021-12-04 20:09:12 -0700

    Housekeeping

--- src/main.rs
+++ src/main.rs
@@ -6,7 +6,6 @@ use std::{
 fn main() {
     let input: Vec<i32> = read_input_file("input.txt").unwrap();
 
-    // let mut larger_measurements: Vec<i32> = Vec::with_capacity(7);
     let mut depth_counter = 0;
 
     for i in 1..input.len() - 2 {
@@ -14,12 +13,9 @@ fn main() {
         let curr = input[i] + input[i + 1] + input[i + 2];
         if prev < curr {
             depth_counter += 1;
-            // larger_measurements.push(input[i + 1])
         }
     }
 
-    // write_output_file("output.txt", larger_measurements).unwrap();
-    // write("output.txt", larger_measurements.len().to_string()).unwrap();
     write("output.txt", depth_counter.to_string()).unwrap();
 }
 
@@ -30,13 +26,4 @@ fn read_input_file(path: &str) -> Result<Vec<i32>, Error> {
         .flatten()
         .collect::<Vec<i32>>();
     Ok(input)
-}
-
-fn write_output_file(path: &str, data: Vec<i32>) -> Result<(), Error> {
-    let output = data
-        .iter()
-        .map(i32::to_string)
-        .collect::<Vec<String>>()
-        .join("\n");
-    return write(path, output);
 }
